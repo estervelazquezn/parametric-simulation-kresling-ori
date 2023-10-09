b3afc6d7b2e1d65171974c14458324817868f6a8
estervelazquezn <79715453+estervelazquezn@users.noreply.github.com>
2023-10-09 16:11:05 +0200

    Add files via upload

--- _runAbq.py
+++ _runAbq.py
@@ -18,37 +18,43 @@ Run Abq from Python
 """
 
 import numpy as np
+import _kreslingGeometry
 
-data = np.loadtxt('_sampling_data.txt',delimiter=' ')
+n = 6
+h = 45
+R = 50
 
+geoInputs = {
+'n': n, 'R': R, 'h': h
+}
 
-for i in range(len(data)-1):
-    i = i + 1
+(geoOutputs) = _kreslingGeometry.Geometry(geoInputs)
+print("h3 = ", geoOutputs['h3'])
 
-    # Parametric data from txt
-    n = int(data[i][0])
-    R = data[i][1]
-    h = data[i][2]
-    xH = data[i][3]
-    a = data[i][4]
-    h3 = data[i][5]
-    rho = data[i][6]
+# Parametric data from txt
+n = geoOutputs['n']
+R = geoOutputs['R']
+h = geoOutputs['h']
+xH = geoOutputs['xH']
+a = geoOutputs['a']
+h3 = geoOutputs['h3']
+rho = geoOutputs['delta']
 
-    # Additional data
-    nMesh = 20
-    N = 2
-    period = 0.01
-    nIntervals = period/50
-    thickness = 0.5
-    Dist = 5
-    materialId = 'TPU'
-    folderCAE = '_CAE/'
-    step = 'FOLD'
+# Additional data
+nMesh = 20
+N = 2
+period = 0.01
+nIntervals = period/50
+thickness = 0.5
+Dist = 5
+materialId = 'TPU'
+folderCAE = '_CAE/'
+step = 'FOLD'
 
-    # Define the file name
-    jobName = str(i) + '-' + step + '-n-' + str(n) + '-h-' + str(int(h*100)) + 'e-2'
-    print(jobName)
+# Define the file name
+jobName = '0-' + step + '-n-' + str(n) + '-h-' + str(int(h*100)) + 'e-2'
+print(jobName)
 
-    import _geo2abq
-    # Run the main function
-    _geo2abq.run(n,R,h, xH, a, h3, rho,jobName,nMesh,period,materialId,N,thickness,folderCAE,step,nIntervals,Dist)
+import _geo2abq
+# Run the main function
+_geo2abq.run(n,R,h, xH, a, h3, rho,jobName,nMesh,period,materialId,N,thickness,folderCAE,step,nIntervals,Dist)
